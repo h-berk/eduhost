b9215bfea424684510615a1d0341c23d47b16bdc
Hasan Berk <hasanberk50@gmail.com>
2024-01-28 15:31:17 +0000

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Welcome to EduHost.</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We provide the website hosting for students, from students.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="App-button">
+          Get Started
+        </button>
       </header>
     </div>
-  );
+  ); 
 }
 
 export default App;
